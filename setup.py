79fdf791cde3dca11975e9379ddd3f0a6578ac31
Daniel Felipe Morales Pinzon <danielf.moralesp20@gmail.com>
2022-06-30 22:03:17 -0500

    feature: create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Machine Learning Project to recognize digits.",
+    author="Daniel Morales P.",
+    license="MIT",
+)
